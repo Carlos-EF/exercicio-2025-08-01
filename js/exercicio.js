d5461a0272ab2f7fe20f852cd1e92e00e19dd98b
Carlos Eduardo Felix Junior <juhfelixx07@gmail.com>
2025-08-03 13:58:50 -0300

    Começado exercício de JS.

---
+++ js/exercicio.js
@@ -0,0 +1,146 @@
+// Exercício 1
+
+function apresentarExercicio1 (){
+    console.log ("SuperDev 7ª Edição");
+}
+
+function apresentarE1Usuario (){
+    alert ("SuperDev 7ª Edição");
+}
+
+// Exercício 1
+
+// Exercício 2
+
+function apresentarExercicio2 (){ 
+    let nome = "Carlos";
+    let nomeComposto = "Eduardo";
+    let sobrenome = "Felix";
+    let nomeCompleto = nome + " " + nomeComposto + " " + sobrenome;
+    let idade = 21;
+    let peso = 56.00;
+    let altura = 1.73;
+    console.log (
+        "Nome: " + nomeCompleto + 
+        "\nIdade: " + idade + " " + "anos" + 
+        "\nPeso: " + peso + " " + "quilos" +
+        "\nAltura: " + altura + "m");
+}
+
+// Exercício 2
+
+// Exercício 3
+
+function apresentarExercicio3 (){
+    let personagem = "Aatrox";
+    let raca = "Darkin";
+    let skillQ = "A Espada Darkin";
+    let skillW = "Correntes Infernais";
+    let skillE = "Avanço Umbral";
+    let skillR = "Aniquilidor de Mundo";
+    let origem = "Runeterra";
+    let rota = "Topo";
+    console.log (
+        "Personagem: " + personagem +
+        "\nRaca: " + raca +
+        "\nSkill Q: " + skillQ +
+        "\nSkill W: " + skillW +
+        "\nSkill E: " + skillE +
+        "\nUltimate: " + skillR +
+        "\nLugar de Origem: " + origem +
+        "\nRota Jogada: " + rota); 
+    
+
+    let personagem2 = "LeBlanc";
+    let raca2 = "Humana";
+    let skill2Q = "Sigilo de Malícia";
+    let skill2W = "Distorção";
+    let skill2E = "Correntes Etéreas";
+    let skill2R = "Mímica";
+    let origem2 = "Noxus";
+    let rota2 = "Meio";
+    console.log (
+        "Personagem: " + personagem2 +
+        "\nRaca: " + raca2 +
+        "\nSkill Q: " + skill2Q +
+        "\nSkill W: " + skill2W +
+        "\nSkill E: " + skill2E +
+        "\nUltimate: " + skill2R +
+        "\nLugar de Origem: " + origem2 +
+        "\nRota Jogada: " + rota2);
+    
+    let personagem3 = "Caitlyn";
+    let raca3 = "Humana";
+    let skill3Q = "Pacificadora de Piltover";
+    let skill3W = "Armadilha Mecânica de Yordle";
+    let skill3E = "Rede Calibre 90";
+    let skill3R = "Ás na Manga";
+    let origem3 = "Piltover";
+    let rota3 = "Atirador";
+    console.log (
+        "Personagem: " + personagem3 +
+        "\nRaca: " + raca3 +
+        "\nSkill Q: " + skill3Q +
+        "\nSkill W: " + skill3W +
+        "\nSkill E: " + skill3E +
+        "\nUltimate: " + skill3R +
+        "\nLugar de Origem: " + origem3 +
+        "\nRota Jogada: " + rota3);
+}
+
+function apresentarE3Usuario (){
+       let personagem = "Aatrox";
+    let raca = "Darkin";
+    let skillQ = "A Espada Darkin";
+    let skillW = "Correntes Infernais";
+    let skillE = "Avanço Umbral";
+    let skillR = "Aniquilidor de Mundo";
+    let origem = "Runeterra";
+    let rota = "Topo";
+    alert (
+        "Personagem: " + personagem +
+        "\nRaca: " + raca +
+        "\nSkill Q: " + skillQ +
+        "\nSkill W: " + skillW +
+        "\nSkill E: " + skillE +
+        "\nUltimate: " + skillR +
+        "\nLugar de Origem: " + origem +
+        "\nRota Jogada: " + rota); 
+    
+
+    let personagem2 = "LeBlanc";
+    let raca2 = "Humana";
+    let skill2Q = "Sigilo de Malícia";
+    let skill2W = "Distorção";
+    let skill2E = "Correntes Etéreas";
+    let skill2R = "Mímica";
+    let origem2 = "Noxus";
+    let rota2 = "Meio";
+    alert (
+        "Personagem: " + personagem2 +
+        "\nRaca: " + raca2 +
+        "\nSkill Q: " + skill2Q +
+        "\nSkill W: " + skill2W +
+        "\nSkill E: " + skill2E +
+        "\nUltimate: " + skill2R +
+        "\nLugar de Origem: " + origem2 +
+        "\nRota Jogada: " + rota2);
+    
+    let personagem3 = "Caitlyn";
+    let raca3 = "Humana";
+    let skill3Q = "Pacificadora de Piltover";
+    let skill3W = "Armadilha Mecânica de Yordle";
+    let skill3E = "Rede Calibre 90";
+    let skill3R = "Ás na Manga";
+    let origem3 = "Piltover";
+    let rota3 = "Atirador";
+    alert (
+        "Personagem: " + personagem3 +
+        "\nRaca: " + raca3 +
+        "\nSkill Q: " + skill3Q +
+        "\nSkill W: " + skill3W +
+        "\nSkill E: " + skill3E +
+        "\nUltimate: " + skill3R +
+        "\nLugar de Origem: " + origem3 +
+        "\nRota Jogada: " + rota3);
+}
